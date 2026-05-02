0750086ca05803775b29a06a1774ad3ff85bc7f6
Fylcoast <fylcoast@gmail.com>
2026-05-02 16:43:10 -0400

    maybe robot done? new item images, new _mapping data, new items, locations, and layouts, for robot

--- scripts/logic/all_episodes.lua
+++ scripts/logic/all_episodes.lua
@@ -1,7 +1,9 @@
 require("scripts/logic/warrior_episodes")
 require("scripts/logic/thief_episodes")
+require("scripts/logic/robot_episodes")
 
 ALL_EPISODES = {
     ['warrior'] = WARRIOR_EPISODES,
-    ['thief'] = THIEF_EPISODES
+    ['thief'] = THIEF_EPISODES,
+    ['robot'] = ROBOT_EPISODES
 }
---
+++ scripts/logic/robot_episodes.lua
@@ -0,0 +1,15 @@
+require("scripts/logic/Robot Episode 1")
+require("scripts/logic/Robot Episode 2")
+require("scripts/logic/Robot Episode 3")
+require("scripts/logic/Robot Episode 4")
+require("scripts/logic/Robot Episode 5")
+require("scripts/logic/Robot Episode 6")
+
+ROBOT_EPISODES = {
+    [1] = ROBOT_EPISODE_ONE,
+    [2] = ROBOT_EPISODE_TWO,
+    [3] = ROBOT_EPISODE_THREE,
+    [4] = ROBOT_EPISODE_FOUR,
+    [5] = ROBOT_EPISODE_FIVE,
+    [6] = ROBOT_EPISODE_SIX
+}
